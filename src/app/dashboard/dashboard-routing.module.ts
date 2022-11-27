c64c47ba6945abba88c3644d0038eef243b87fe4
ozpinar <ozpinarberk@gmail.com>
2022-11-28 01:22:14 +0300

    [feat] Create game component

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { AddGameComponent } from './pages/add-game/add-game.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
+  { path: 'add-game', component: AddGameComponent },
 ];
 
 @NgModule({
